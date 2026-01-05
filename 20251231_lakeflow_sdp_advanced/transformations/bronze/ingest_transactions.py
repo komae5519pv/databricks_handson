dae238ff3c572eb5faec73bd5b66a7721d1b2b29
komae5519pv <konomi0572@gmail.com>
2026-01-05 03:33:48 +0000

    delete comment

--- 20251231_lakeflow_sdp_advanced/transformations/bronze/ingest_transactions.py
+++ 20251231_lakeflow_sdp_advanced/transformations/bronze/ingest_transactions.py
@@ -17,11 +17,8 @@ def transactions_east():
       .option("cloudFiles.format", "csv")
       .option("cloudFiles.inferColumnTypes", "true")
       # --- スキーマ進化 (Evolution) 設定 ---
-      # 新カラム検出時はストリームが一旦停止し、新規カラムを追加いsて再起動して自動取り込み（Jobsの自動再起動を有効化しておく）
       .option("cloudFiles.schemaEvolutionMode", "addNewColumns")
-      # 救済カラム設定/解析不可の壊れたデータや型不一致はこのカラムにJSONとして退避（列追加とは両立）
       .option("cloudFiles.rescuedDataColumn", "_rescued_data_transactions")
-      # （推奨）スキーマ保存パス：ソース/フローごとに分けると衝突回避やデバッグが容易
       .option("cloudFiles.schemaLocation",
         f"/Volumes/{catalog_name}/{schema_name}/{volume_name}/_schemas/transactions_east")
       .load(f"/Volumes/{catalog_name}/{schema_name}/{volume_name}/transactions_east/")
@@ -35,11 +32,8 @@ def transactions_west():
       .option("cloudFiles.format", "csv")
       .option("cloudFiles.inferColumnTypes", "true")
       # --- スキーマ進化 (Evolution) 設定 ---
-      # 新カラム検出時はストリームが一旦停止し、新規カラムを追加して再起動して自動取り込み（Jobsの自動再起動を有効化しておく）
       .option("cloudFiles.schemaEvolutionMode", "addNewColumns")
-      # 救済カラム設定/解析不可の壊れたデータや型不一致はこのカラムにJSONとして退避（列追加とは両立）
       .option("cloudFiles.rescuedDataColumn", "_rescued_data_transactions")
-      # （推奨）スキーマ保存パス：ソース/フローごとに分けると衝突回避やデバッグが容易
       .option("cloudFiles.schemaLocation",
         f"/Volumes/{catalog_name}/{schema_name}/{volume_name}/_schemas/transactions_west")
       .load(f"/Volumes/{catalog_name}/{schema_name}/{volume_name}/transactions_west/")
